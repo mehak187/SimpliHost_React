02a79572c7f9df776f16b455568546fe3e6a2004
shakaib-fabtechsol <shakaib@fabtechsol.com>
2024-10-04 12:54:00 +0500

    properties

---
+++ src/Components/Properties/PropertyGroups.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PropertyGroups() {
+  return (
+    <div>
+      Property Groups
+    </div>
+  )
+}
+
+export default PropertyGroups
--- src/Pages/Properties.jsx
+++ src/Pages/Properties.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React from "react";
+import AllProperties from "../Components/Properties/AllProperties";
+import PropertyGroups from "../Components/Properties/PropertyGroups";
 
 function Properties() {
   return (
-    <div>Properties</div>
-  )
+    <section>
+      <div className="container-fluid">
+        <div>
+          <nav>
+            <div
+              className="nav nav-tabs border-0 mt-3"
+              id="nav-tab"
+              role="tablist"
+            >
+              <button
+                className="nav-link active"
+                id="Properties-tab"
+                data-bs-toggle="tab"
+                data-bs-target="#Properties"
+                type="button"
+                role="tab"
+                aria-controls="Properties"
+                aria-selected="true"
+              >
+                Properties
+              </button>
+              <button
+                className="nav-link"
+                id="PropertyGroups-tab"
+                data-bs-toggle="tab"
+                data-bs-target="#PropertyGroups"
+                type="button"
+                role="tab"
+                aria-controls="PropertyGroups"
+                aria-selected="false"
+              >
+                Property Groups
+              </button>
+            </div>
+            <div className="tab-content mt-3" id="nav-tabContent">
+              <div
+                className="tab-pane fade show active"
+                id="Properties"
+                role="tabpanel"
+                aria-labelledby="Properties-tab"
+                tabIndex={0}
+              >
+                <AllProperties />
+              </div>
+              <div
+                className="tab-pane fade"
+                id="PropertyGroups"
+                role="tabpanel"
+                aria-labelledby="PropertyGroups-tab"
+                tabIndex={0}
+              >
+                <PropertyGroups />
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Properties
+export default Properties;
