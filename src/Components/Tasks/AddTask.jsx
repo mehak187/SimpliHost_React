200bab43b37313495390e705259a3b799f3a5d77
mehak-fabtechsol <mehak@fabtechsol.com>
2024-10-06 15:06:35 +0500

    tasks

--- src/Components/Tasks/AddTask.jsx
+++ src/Components/Tasks/AddTask.jsx
@@ -9,13 +9,13 @@ function AddTask() {
         </div>
         <form action="">
           <div className="row">
-            <div className="col-lg-8">
+            <div className="col-lg-12 col-xl-10 col-xxl-8">
               <div className="row">
-                <div className="col-md-6 mt-3">
+                <div className="col-lg-6 mt-3">
                   <label htmlFor="TaskName" className='fw-semi'>Task Name</label><br />
                   <input type="text" id='TaskName' className='inputstyle focus-none' placeholder='Type name here'/>
                 </div>
-                <div className="col-md-6 mt-3">
+                <div className="col-lg-6 mt-3">
                   <label htmlFor="Property" className='fw-semi'>Property</label><br />
                   <select name="Property" id="Property" className='inputstyle focus-none'>
                     <option value="" selected disabled hidden>Select Property</option>
@@ -24,7 +24,7 @@ function AddTask() {
                     <option value="Property3">Property3</option>
                   </select>
                 </div>
-                <div className="col-md-6 mt-3">
+                <div className="col-lg-6 mt-3">
                   <label htmlFor="TaskType" className='fw-semi'>Select Task Type</label><br />
                   <select name="TaskType" id="TaskType" className='inputstyle focus-none'>
                     <option value="" selected disabled hidden>Select Task Type</option>
@@ -33,7 +33,7 @@ function AddTask() {
                     <option value="Task3">Task3</option>
                   </select>
                 </div>
-                <div className="col-md-6 mt-3">
+                <div className="col-lg-6 mt-3">
                   <label htmlFor="Assign" className='fw-semi'>Assign To</label><br />
                   <select name="Assign" id="Assign" className='inputstyle focus-none'>
                     <option value="" selected disabled hidden>Select Assign To</option>
@@ -42,26 +42,42 @@ function AddTask() {
                     <option value="Assign3">Assign3</option>
                   </select>
                 </div>
-                <div className="col-md-6 mt-3">
+                <div className="col-lg-6 mt-3">
                   <label htmlFor="StartDate" className='fw-semi'>Select Start Date</label><br />
                   <input type="date" id='StartDate' className='inputstyle focus-none'/>
                 </div>
-                <div className="col-md-6 mt-3">
+                <div className="col-lg-6 mt-3">
                   <label htmlFor="StartTime" className='fw-semi'>Select Start Time</label><br />
                   <input type="time" id='StartTime' className='inputstyle focus-none'/>
                 </div>
-                <div className="col-md-6 mt-3">
+                <div className="col-lg-6 mt-3">
                   <label htmlFor="EndDate" className='fw-semi'>Select End Date</label><br />
                   <input type="date" id='EndDate' className='inputstyle focus-none'/>
                 </div>
-                <div className="col-md-6 mt-3">
+                <div className="col-lg-6 mt-3">
                   <label htmlFor="EndTime" className='fw-semi'>Select End Time</label><br />
                   <input type="time" id='EndTime' className='inputstyle focus-none'/>
                 </div>
-                <div className="col-md-6 mt-3">
-                  
+                <div className="col-lg-6 mt-3">
+                  <label htmlFor="EndTime" className='fw-semi'>Select Inventory</label><br />
+                  <div className="d-sm-flex align-items-center">
+                    <div className="d-flex align-items-center">
+                      <label htmlFor="Materials" className='me-2'>Repeating?</label>
+                      <label className="switch me-3">
+                        <input type="checkbox" id='Materials'/>
+                        <span className="slider round"></span>
+                      </label>
+                    </div>
+                    <div className="d-flex mt-2 mt-sm-0 align-items-center">
+                        <input type="number" id='' placeholder="00" className='inputstyle me-2 w-100 focus-none' />
+                        <select name="" id="" className='inputstyle w-100 focus-none'>
+                          <option value="days" >Days</option>
+                          <option value="weeks">Weeks</option>
+                        </select>
+                    </div>
+                  </div>
                 </div>
-                <div className="col-md-6 mt-3">
+                <div className="col-lg-6 mt-3">
                   <label htmlFor="Checklist" className='fw-semi'>Select Checklist</label><br />
                   <select name="Checklist" id="Checklist" className='inputstyle focus-none'>
                     <option value="" selected disabled hidden>Select Select Checklist</option>
