3759918a86960f7a4da2722b5f5eabd52b097bea
mehak-fabtechsol <mehak@fabtechsol.com>
2024-10-09 10:36:24 +0500

    not

--- src/Pages/Notifications.jsx
+++ src/Pages/Notifications.jsx
@@ -46,18 +46,18 @@ function Notifications() {
           {Notifications.map((data) => (
             <div className="row my-3">
               <div className="col-12 notif p-3">
-                <div className="d-flex justify-content-between align-items-center">
+                <div className="d-lg-flex justify-content-between align-items-center">
                   <div className="d-flex align-items-center">
-                    <img src={bells} alt="" className=" me-3" />
+                    <img src={bells} alt="" className="bell d-none d-sm-block me-2 me-md-3" />
                     <div>
                       <p className="mb-0 fw-semi">{data.Title}</p>
                       <p className="mb-0 small">{data.Content}</p>
                     </div>
                   </div>
-                  <div>
-                    <div className="d-flex justify-content-end align-items-center">
-                      <p className="mb-0 small me-4">{data.Date}</p>
-                      <p className="mb-0 small">{data.Time}</p>
+                  <div className="mt-3 mt-lg-0">
+                    <div className="d-flex flex-wrap justify-content-end align-items-center">
+                      <p className="mb-0 small">{data.Date}</p>
+                      <p className="mb-0 small  ms-2 ms-sm-3 ms-md-4">{data.Time}</p>
                     </div>
                     <div className=" d-flex align-items-center justify-content-end mt-3">
                       <span>Off</span>
