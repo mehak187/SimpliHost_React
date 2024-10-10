82e5cbf69787bac29a002858eae50a75f6f0b1c1
shakaib-fabtechsol <shakaib@fabtechsol.com>
2024-10-10 14:31:51 +0500

    done

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,16 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+
+
+import { PrimeReactProvider } from 'primereact/api';
+        
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <PrimeReactProvider>
     <App />
+    </PrimeReactProvider>
   </StrictMode>,
 )
