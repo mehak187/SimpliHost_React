efdefe43476fd14663ae981381d707a436132319
mehak-fabtechsol <mehak@fabtechsol.com>
2024-10-14 15:33:19 +0500

    css

--- src/Components/Website/Listings.jsx
+++ src/Components/Website/Listings.jsx
@@ -15,70 +15,86 @@ import detailImage from "../../assets/img/furnished-deatil.svg";
 function Listings() {
   return (
     <>
-      <div className="container py-5">
-        <div className="col-12">
-          <h1 className="fw-bold black-color text-center">
-            Find Your Perfect Getaway!
-          </h1>
-          <div className="d-flex justify-content-center">
-            <p className="mb-0 mt-3 listinghero-para  text-center">
-              Searching for a spot to unwind? Let us help you discover the best
-              properties that feel just like home—or maybe even better!
-            </p>
-          </div>
-          <div className="mt-5">
-            <form>
-              <div className="d-flex justify-content-between listingform-bg rounded-4 p-3">
-                <div className="searchform-group listingborder-right">
-                  <label htmlFor="location">Enter Location:</label>
-                  <input
-                    type="text"
-                    id="location"
-                    name="location"
-                    placeholder="Enter a city"
-                    className="border-0 listingsearh-bg "
-                  />
-                </div>
-                <div className="searchform-group listingborder-right">
-                  <label htmlFor="checkindate">Check In:</label>
-                  <input
-                    type="date"
-                    id="checkindate"
-                    name="checkIn-date"
-                    placeholder="Add Dates"
-                    className="border-0 listingsearh-bg"
-                  />
-                </div>
-                <div className="searchform-group listingborder-right">
-                  <label htmlFor="checkoutdate">Check out:</label>
-                  <input
-                    type="date"
-                    id="checkoutdate"
-                    name="checkout-date"
-                    placeholder="Add Dates"
-                    className="border-0 listingsearh-bg"
-                  />
-                </div>
-                <div className="searchform-group">
-                  <label htmlFor="guest">Guests:</label>
-                  <input
-                    type="text"
-                    id="guest"
-                    name="Guest"
-                    placeholder="Add Guest"
-                    className="border-0 listingsearh-bg"
-                  />
-                </div>
-                <a href="#" className="text-decoration-none">
-                  <div className="listingsearchicon-size">
-                    <img src={faserach} alt="search" className="search-icon" />
-                  </div>
-                </a>
-              </div>
-            </form>
+      <section className="listing-color  py-5">
+        <div className="container">
+          <div className="col-12">
+            <h1 className="fw-bold black-color text-center">
+              Find Your Perfect Gateway!
+            </h1>
+            <div className="d-flex justify-content-center">
+              <p className="mb-0 mt-3 listinghero-para  text-center">
+                Searching for a spot to unwind? Let us help you discover the
+                best properties that feel just like home—or maybe even better!
+              </p>
+            </div>
           </div>
         </div>
-      </div>
+        <div className="container-fluid">
+            <div className="mt-5">
+              <form>
+                <div className="row justify-content-center">
+                  <div className="col-md-11 col-lg-10 col-xl-9 col-xxl-7">
+                    <div className="d-flex justify-content-between listingform-bg rounded-4 p-3">
+                      <div className="col-md-3 my-2 my-md-0 pe-3 searchform-group ">
+                        <label htmlFor="location">Enter Location:</label>
+                        <input
+                          type="text"
+                          id="location"
+                          name="location"
+                          placeholder="Enter a city"
+                          className="border-0 listingsearh-bg pt-0 pt-md-2 w-100 pb-0 "
+                        />
+                      </div>
+                      <div className="col-md-3 my-2 my-md-0 pe-3 searchform-group border-start border-1 ps-md-3">
+                        <label htmlFor="checkindate">Check In:</label>
+                        <input
+                          type="date"
+                          id="checkindate"
+                          name="checkIn-date"
+                          placeholder="Add Dates"
+                          className="border-0 listingsearh-bg pt-0 pt-md-2 w-100 pb-0"
+                        />
+                      </div>
+                      <div className="col-md-3 my-2 my-md-0 pe-3 searchform-group border-start border-1 ps-md-3">
+                        <label htmlFor="checkoutdate">Check out:</label>
+                        <input
+                          type="date"
+                          id="checkoutdate"
+                          name="checkout-date"
+                          placeholder="Add Dates"
+                          className="border-0 listingsearh-bg pt-0 pt-md-2 w-100 pb-0"
+                        />
+                      </div>
+                      <div className="col-md-3 my-2 my-md-0 d-flex justify-content-between align-items-center">
+                        <div className=" searchform-group  border-start border-1 ps-md-3">
+                          <label htmlFor="guest">Guests:</label>
+                          <input
+                            type="text"
+                            id="guest"
+                            name="Guest"
+                            placeholder="Add Guest"
+                            className="border-0 listingsearh-bg pt-0 pt-md-2 w-100 pb-0"
+                          />
+                        </div>
+                        <div className="">
+                          <a href="#" className="text-decoration-none">
+                            <div className="listingsearchicon-size">
+                              <img
+                                src={faserach}
+                                alt="search"
+                                className="search-icon"
+                              />
+                            </div>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+      </section>
       <section className="listing-second py-5">
         <div className="container">
           <div className="row">
