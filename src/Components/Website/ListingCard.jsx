ea116b366383bc0913bb2f63526b11cee8e79dcf
mehak-fabtechsol <mehak@fabtechsol.com>
2024-10-17 15:38:21 +0500

    listings

--- src/Components/Website/ListingCard.jsx
+++ src/Components/Website/ListingCard.jsx
@@ -8,13 +8,17 @@ import bedimg from "../../assets/img/furnished-bed.svg";
 import furniture2img from "../../assets/img/furniture2.svg";
 import carimg from "../../assets/img/furnished-car.svg";
 import furniture4 from "../../assets/img/furniture4.svg";
+import FurnishFlower from "../../assets/img/furnishFlower.png";
 import { Link } from "react-router-dom";
 import { FaRegHeart } from "react-icons/fa";
+import Arrows from "../../assets/img/arrow2.png";
+
 const listings = [
   {
     imageSrcs: [PCI, PCI, PCI, PCI, PCI, PCI],
     title: "Well Furnished Apartment1",
     location: "100 Smart Street, LA, USA",
+    flowerSrc: FurnishFlower,
     beds: 3,
     baths: 2,
     cars: 1,
@@ -24,6 +28,7 @@ const listings = [
     imageSrcs: [PCI, PCI, PCI, PCI, PCI, PCI],
     title: "Well Furnished Apartment 2",
     location: "100 Smart Street, LA, USA",
+    flowerSrc: FurnishFlower,
     beds: 3,
     baths: 2,
     cars: 1,
@@ -33,6 +38,7 @@ const listings = [
     imageSrcs: [PCI, PCI, PCI, PCI, PCI, PCI],
     title: "Well Furnished Apartment 3",
     location: "100 Smart Street, LA, USA",
+    flowerSrc: FurnishFlower,
     beds: 3,
     baths: 2,
     cars: 1,
@@ -42,6 +48,7 @@ const listings = [
     imageSrcs: [PCI, PCI, PCI, PCI, PCI, PCI],
     title: "Well Furnished Apartment 4",
     location: "100 Smart Street, LA, USA",
+    flowerSrc: FurnishFlower,
     beds: 3,
     baths: 2,
     cars: 1,
@@ -58,6 +65,8 @@ const ListingCard = () => {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
     prevArrow: (
       <div className="slider-arrow1 slider-prev">
         <img src={Left} alt="left" className="ar-img shadow" />
@@ -70,7 +79,7 @@ const ListingCard = () => {
     ),
     responsive: [
       {
-        breakpoint: 1200,
+        breakpoint: 992,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
@@ -78,7 +87,7 @@ const ListingCard = () => {
         },
       },
       {
-        breakpoint: 600,
+        breakpoint: 768,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
@@ -111,7 +120,7 @@ const ListingCard = () => {
     <div className="slides">
       <Slider {...settingsOuter}>
         {listings.map((listing, index) => (
-          <div className="col-12 mt-4 mx-auto px-sm-4 px-2 py-5" key={index}>
+          <div className="col-12 mt-md-4 mx-auto px-sm-4 px-2 py-5" key={index}>
             <div className="p-3 furnished-shadow inner-slider position-relative rounded-3">
               <Slider {...settingsInner}>
                 {listing.imageSrcs.map((src, idx) => (
@@ -123,13 +132,21 @@ const ListingCard = () => {
                   />
                 ))}
               </Slider>
+
               <Link to="#" className="furnishedover-size">
-                <FaRegHeart className=" text-black"/>
+                <FaRegHeart className=" text-black" />
               </Link>
-              <div className="d-flex align-items-center mt-3">
+              <div className="d-flex justify-content-center justify-content-md-start align-items-center mt-3">
+                <div className="furnished-flower d-none d-sm-block">
+                  <img
+                    src={listing.flowerSrc}
+                    alt="furnished-flower"
+                    className="w-100"
+                  />
+                </div>
                 <p className="mb-0 fs-6 fw-bold ms-2">{listing.title}</p>
               </div>
-              <div className="d-flex align-items-center mt-3">
+              <div className="d-flex justify-content-center justify-content-md-start align-items-center mt-3">
                 <div className="">
                   <img src={locationimg} alt="location" className="w-100" />
                 </div>
@@ -137,15 +154,15 @@ const ListingCard = () => {
                   {listing.location}
                 </p>
               </div>
-              <div className="d-flex justify-content-between mt-3">
+              <div className="d-flex flex-column flex-md-row align-items-center justify-content-between mt-3">
                 <div className="d-flex align-items-center">
-                <div className="d-flex align-items-center">
+                  <div className="d-flex align-items-center">
                     <div className="furniture-size">
                       <img src={bedimg} alt="bed" className="sm-img" />
                     </div>
-                    <p className="mb-0 fw-bold ms-2">{listing.beds}</p>
+                    <p className="mb-0 fw-bold ms-1">{listing.beds}</p>
                   </div>
-                  <div className="d-flex align-items-center ms-1">
+                  <div className="d-flex align-items-center ms-2">
                     <div className="furniture-size">
                       <img
                         src={furniture2img}
@@ -153,15 +170,15 @@ const ListingCard = () => {
                         className="sm-img"
                       />
                     </div>
-                    <p className="mb-0 fw-bold ms-2">{listing.baths}</p>
+                    <p className="mb-0 fw-bold ms-1">{listing.baths}</p>
                   </div>
-                  <div className="d-flex align-items-center ms-1">
+                  <div className="d-flex align-items-center ms-2">
                     <div className="furniture-size">
                       <img src={carimg} alt="car" className="sm-img" />
                     </div>
-                    <p className="mb-0 fw-bold ms-2">{listing.cars}</p>
+                    <p className="mb-0 fw-bold ms-1">{listing.cars}</p>
                   </div>
-                  <div className="d-flex align-items-center ms-1">
+                  <div className="d-flex align-items-center ms-2">
                     <div className="furniture-size">
                       <img
                         src={furniture4}
@@ -169,9 +186,16 @@ const ListingCard = () => {
                         className="sm-img"
                       />
                     </div>
-                    <p className="mb-0 fw-bold ms-2">{listing.garage}</p>
+                    <p className="mb-0 fw-bold ms-1">{listing.garage}</p>
                   </div>
                 </div>
+                <Link
+                  to="/listing-details"
+                  className="detail-btn mt-3 mt-md-0 text-white d-inline-flex align-items-center text-decoration-none"
+                >
+                  Detail
+                  <img src={Arrows} alt="detail" className="ms-2" />
+                </Link>
               </div>
             </div>
           </div>
